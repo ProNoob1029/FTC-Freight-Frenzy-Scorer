c6dd39489bbac6d862df6135610733f778caedaa
ProNoob1029 <pronoob1029@gmail.com>
2021-12-09 16:53:00 +0200

    new activity and stuff

--- app/src/main/java/com/example/ftc_freightfrenzy_scorer/MainActivity.java
+++ app/src/main/java/com/example/ftc_freightfrenzy_scorer/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ftc_freightfrenzy_scorer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
@@ -14,13 +15,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button buttonNew = findViewById(R.id.button_new);
         Vibrator myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+        Button buttonNew = (Button) findViewById(R.id.button_new);
+        Button buttonList = (Button) findViewById(R.id.button_list);
 
-        buttonNew.setOnClickListener(
-            v -> {
+        buttonNew.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                myVib.vibrate(20);
+                startActivity(new Intent(MainActivity.this, ScorerActiviy.class));
+            }
+        });
+
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 myVib.vibrate(20);
             }
-        );
+        });
     }
 }
