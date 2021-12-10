bc39a773caba3297e220e38170a3e753553d9706
ProNoob1029 <pronoob1029@gmail.com>
2021-12-10 16:09:07 +0200

    nOICE button logic

--- app/src/main/java/com/example/ftc_freightfrenzy_scorer/MainActivity.java
+++ app/src/main/java/com/example/ftc_freightfrenzy_scorer/MainActivity.java
@@ -16,20 +16,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Vibrator myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
-        Button buttonNew = (Button) findViewById(R.id.button_new);
-        Button buttonList = (Button) findViewById(R.id.button_list);
+        Button buttonNew = findViewById(R.id.button_new);
+        Button buttonList = findViewById(R.id.button_list);
 
-        buttonNew.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                myVib.vibrate(20);
-                startActivity(new Intent(MainActivity.this, ScorerActiviy.class));
-            }
+        buttonNew.setOnClickListener(v -> {
+            myVib.vibrate(20);
+            startActivity(new Intent(MainActivity.this, ScorerActiviy.class));
         });
 
-        buttonList.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                myVib.vibrate(20);
-            }
-        });
+        buttonList.setOnClickListener(v -> myVib.vibrate(20));
     }
 }
--- app/src/main/java/com/example/ftc_freightfrenzy_scorer/ScorerActiviy.java
+++ app/src/main/java/com/example/ftc_freightfrenzy_scorer/ScorerActiviy.java
@@ -1,16 +1,74 @@
 package com.example.ftc_freightfrenzy_scorer;
 
+import static java.lang.String.format;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 public class ScorerActiviy extends AppCompatActivity{
 
+    public int autoStorage = 0;
+    public int autoHub = 0;
+
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scorer);
 
+        Vibrator myVib = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+        SwitchCompat switchParked = findViewById(R.id.switch_parked);
+        SwitchCompat switchParkedFully = findViewById(R.id.switch_parked_fully);
+        TextView textAutoStorage = findViewById(R.id.text_auto_storage);
+        TextView textAutoHub = findViewById(R.id.text_auto_hub);
+        Button buttonAutoStoragePlus = findViewById(R.id.button_auto_storage_plus);
+        Button buttonAutoStorageMinus = findViewById(R.id.button_auto_storage_minus);
+        Button buttonAutoHubPlus = findViewById(R.id.button_auto_hub_plus);
+        Button buttonAutoHubMinus = findViewById(R.id.button_auto_hub_minus);
 
+        switchParked.setOnClickListener(v -> {
+            if(!switchParked.isChecked()){
+                switchParkedFully.setVisibility(View.GONE);
+                switchParkedFully.setChecked(false);
+            }
+            else switchParkedFully.setVisibility((View.VISIBLE));
+        });
+
+        buttonAutoStoragePlus.setOnClickListener(v -> {
+            autoStorage++;
+            myVib.vibrate(20);
+            textAutoStorage.setText(format("%d", autoStorage));
+            //textAutoStorage.setText("" + 1);
+        });
+
+        buttonAutoStorageMinus.setOnClickListener(v -> {
+            if(autoStorage > 0){
+                autoStorage--;
+                myVib.vibrate(20);
+                textAutoStorage.setText(format("%d", autoStorage));
+            }
+        });
+
+        buttonAutoHubPlus.setOnClickListener(v -> {
+            autoHub++;
+            myVib.vibrate(20);
+            textAutoHub.setText(format("%d", autoHub));
+            //textAutoStorage.setText("" + 1);
+        });
+
+        buttonAutoHubMinus.setOnClickListener(v -> {
+            if(autoHub > 0){
+                autoHub--;
+                myVib.vibrate(20);
+                textAutoHub.setText(format("%d", autoHub));
+            }
+        });
     }
 }
