b7508558c7463ff8884ea15676deaafb2155f3f3
ProNoob1029 <pronoob1029@gmail.com>
2022-01-05 18:16:04 +0200

    mental breakdown

--- app/src/main/java/com/example/ftc_freight_frenzy_scorer/ListActivity.java
+++ app/src/main/java/com/example/ftc_freight_frenzy_scorer/ListActivity.java
@@ -2,10 +2,11 @@ package com.example.ftc_freight_frenzy_scorer;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentTransaction;
 
-public class ListActivity extends FragmentActivity {
+public class ListActivity extends AppCompatActivity {
 
     //protected ActivityListBinding binding;
 
@@ -17,11 +18,11 @@ public class ListActivity extends FragmentActivity {
         setContentView(view);*/
         setContentView(R.layout.activity_list);
 
-        /*if (savedInstanceState == null) {
+        if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             RecyclerViewFragment fragment = new RecyclerViewFragment();
             transaction.replace(R.id.sample_content_fragment, fragment);
             transaction.commit();
-        }*/
+        }
     }
 }
